f21facc09cc69f1a8b3244a78d5408aa54b6ff82
wooyeol <jww0608@naver.com>
2021-09-02 12:32:40 +0900

    Loading page added

---
+++ app/src/main/java/com/finaltest/startfit/LodingActivity.java
@@ -0,0 +1,30 @@
+package com.finaltest.startfit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LodingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loding);
+        startLoading();
+    }
+
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+    }
+}
