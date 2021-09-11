10beb66fd138f26f16b08b97e2e7f6848fc1bab8
togaso21 <togaso21@naver.com>
2021-09-11 15:42:25 +0900

    video, menu update

---
+++ app/src/main/java/com/finaltest/startfit/VideoView.java
@@ -0,0 +1,100 @@
+package com.finaltest.startfit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Bundle;
+import android.se.omapi.Session;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class VideoView extends YouTubeBaseActivity {
+
+    //객체 선언
+    YouTubePlayerView playerView;
+    YouTubePlayer player;
+    //유튜브 API KEY와 동영상 ID 변수 설정
+    private static String API_KEY = "AIzaSyBFcvJcRToyKOlwNVPVBSfDfkXXnl7wR_0";
+    //https://www.youtube.com/watch?v=hl-ii7W4ITg ▶ 유튜브 동영상 v= 다음 부분이 videoId
+
+
+    //private static String videoId = "kTJczUoc26U";
+    //logcat 사용 설정
+    private static final String TAG = "VideoView";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_view);
+        initPlayer();
+        Button btnPlay = findViewById(R.id.youtubeBtn);
+
+        Intent intent = getIntent();
+        String videoId = intent.getStringExtra("영상");
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(player != null) {
+                    if(player.isPlaying()) {
+                        player.pause();
+                    }
+                    player.cueVideo(videoId);
+                }
+            }
+        });
+
+
+    }
+    /*private void playVideo() {
+        if(player != null) {
+            if(player.isPlaying()) {
+                player.pause();
+            }
+            player.cueVideo(videoId);
+        }
+    }*/
+
+    //유튜브 플레이어 메서드
+    private void initPlayer() {
+        playerView = findViewById(R.id.youTubePlayerView);
+        playerView.initialize(API_KEY, new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                player = youTubePlayer;
+                player.setPlayerStateChangeListener(new YouTubePlayer.PlayerStateChangeListener() {
+                    @Override
+                    public void onLoading() {
+                    }
+                    @Override
+                    public void onLoaded(String id) {
+                        Log.d(TAG, "onLoaded: " + id);
+                        player.play();
+                    }
+                    @Override
+                    public void onAdStarted() {
+                    }
+                    @Override
+                    public void onVideoStarted() {
+                    }
+                    @Override
+                    public void onVideoEnded() {
+                    }
+                    @Override
+                    public void onError(YouTubePlayer.ErrorReason errorReason) {
+                        Log.d(TAG, "onError: " + errorReason);
+                    }
+                });
+            }
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+            }
+        });
+    }
+}
