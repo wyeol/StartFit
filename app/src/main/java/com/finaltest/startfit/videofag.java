ef6031fed04e4d1a28910a79e1f8bbe9b594ae0a
togaso21 <togaso21@naver.com>
2021-09-11 15:41:03 +0900

    video, menu update

--- app/src/main/java/com/finaltest/startfit/videofag.java
+++ app/src/main/java/com/finaltest/startfit/videofag.java
@@ -1,9 +1,12 @@
 package com.finaltest.startfit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,9 +14,10 @@ import androidx.fragment.app.Fragment;
 
 import com.finaltest.startfit.R;
 
-public class videofag extends Fragment {
+public class videofag extends Fragment{
 
     private View view;
+    private Button mbt;
 
     @Nullable
     @Override
@@ -21,6 +25,18 @@ public class videofag extends Fragment {
         view = inflater.inflate(R.layout.video_fag, container, false);
 
 
+        mbt = view.findViewById(R.id.exercise1);
+
+        mbt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), exercise_upper_body.class);
+                startActivity(intent);
+            }
+        });
+
         return view;
     }
+
+
 }
